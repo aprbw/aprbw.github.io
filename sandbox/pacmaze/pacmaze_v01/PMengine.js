c374f148ebba3e8c113847214e23e72d936f19e8
aprbw <42436117+aprbw@users.noreply.github.com>
2020-08-14 22:48:21 +1000

    Update PMengine.js

--- sandbox/pacmaze/pacmaze_v01/PMengine.js
+++ sandbox/pacmaze/pacmaze_v01/PMengine.js
@@ -8,15 +8,17 @@ _actualFPS=-1;
 
 
 var _PM_Gameloop = function(){
+    while(1){
     if(_isNotPaused<0){
         requestAnimationFrame(_PM_Gameloop);
-        return;
+//         return;
     }
     _currentFrame_timestamp=_timestamp();
     var deltaTime = _timestamp()-_lastFrame_timestamp;
     if(deltaTime<_TIMESTEP){
-        _PM_Gameloop();
-        return;
+//         _PM_Gameloop();
+//         return;
+        continue;
     }
     requestAnimationFrame(_PM_Gameloop);
     _canvasElmnt.width = _CANVAS_WIDTH;
@@ -35,5 +37,6 @@ var _PM_Gameloop = function(){
 
 
     $("#display").text(Math.round(_actualFPS)+" "+_frameCount);
+    }
     return;
 }
