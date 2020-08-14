9bb775561e486c7848403a462651fce47442c4db
aprbw <42436117+aprbw@users.noreply.github.com>
2020-08-15 00:16:15 +1000

    Update PMengine.js

--- sandbox/pacmaze/pacmaze_v01/PMengine.js
+++ sandbox/pacmaze/pacmaze_v01/PMengine.js
@@ -6,40 +6,37 @@ _lastFrame_timestamp=_currentFrame_timestamp;
 _frameCount=0;
 _actualFPS=-1;
 
-_iloop = 1000;
 
 var _PM_Gameloop = function(){
-    while(_iloop>0){
-        _iloop--
-        if(_isNotPaused<0){
-            requestAnimationFrame(_PM_Gameloop);
-    //         return;
-        }
-        _currentFrame_timestamp=_timestamp();
-        var deltaTime = _timestamp()-_lastFrame_timestamp;
-        if(deltaTime<_TIMESTEP){
-    //         _PM_Gameloop();
-    //         return;
-            continue;
-        }
-        console.log("d")
+    if(_isNotPaused<0){
         requestAnimationFrame(_PM_Gameloop);
-        _canvasElmnt.width = _CANVAS_WIDTH;
-        _frameCount++;
-        _lastFrame_timestamp=_timestamp();
-        _actualFPS=1000/deltaTime;
-    /*
-        masukanSemua();
-        gameMechanic();
-        gerakSemua();
-        gambarSemua();
-        updateDisplay()
-    */
-        _paku.onFrame();
-
-
-
-        $("#display").text(Math.round(_actualFPS)+" "+_frameCount);
+        // return;
     }
+    _currentFrame_timestamp=_timestamp();
+    var deltaTime = _timestamp()-_lastFrame_timestamp;
+    while(deltaTime < _TIMESTEP){
+        deltaTime = _timestamp()-_lastFrame_timestamp;
+    }
+    // if(deltaTime<_TIMESTEP){
+    //     // _PM_Gameloop();
+    //     return;
+    // }
+    requestAnimationFrame(_PM_Gameloop);
+    _canvasElmnt.width = _CANVAS_WIDTH;
+    _frameCount++;
+    _lastFrame_timestamp=_timestamp();
+    _actualFPS=1000/deltaTime;
+/*
+    masukanSemua();
+    gameMechanic();
+    gerakSemua();
+    gambarSemua();
+    updateDisplay()
+*/
+    _paku.onFrame();
+
+
+
+    $("#display").text(Math.round(_actualFPS)+" "+_frameCount);
     return;
 }
